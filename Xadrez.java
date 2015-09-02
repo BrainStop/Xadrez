190f02983e2b53ab8d95744cda60b881586bb25c
D-eject <vitor3melo@outlook.pt>
2015-09-02 15:54:56 +0100

    Create Xadrez.java

---
+++ Xadrez.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Xadrez {
+    public static void main (String args[]) {
+        Random rn = new Random();
+        char [][] chessboard = new char [8] [8];
+        
+    }
+}
